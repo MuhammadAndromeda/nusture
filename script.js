9342379b3eda017ded464b655df2bfef071358e0
andromeda <q.a.r.h.hrp0204@gmail.com>
2025-01-22 23:28:14 +0700

    commit

---
+++ script.js
@@ -0,0 +1,69 @@
+function sumatera(){
+    const pulau = document.getElementById('pulau');
+    const info = document.getElementById('info');
+    const budaya = document.getElementById('budaya');
+    const suku1 = document.getElementById('suku1');
+    const suku2 = document.getElementById('suku2');
+
+    pulau.textContent = '📍 SUMATERA ISLAND';
+    info.textContent = 'Sumatera adalah pulau keenam terbesar di dunia yang terletak di Indonesia, dengan luas 473.481 km². asal nama Sumatra berawal dari keberadaaan kata Samudra yang merujuk pada Kesultanan Samudera Pasai di pesisir timur Aceh.'
+    budaya.textContent = 'Sumatera dikenal sebagai pulau yang kaya akan keberagaman suku dan budaya, seperti suku Batak, Minangkabau, Melayu, Aceh, dan lainnya, yang memiliki tradisi unik dan khas. Pulau ini juga menawarkan berbagai destinasi wisata, mulai dari Danau Toba yang megah, Jam Gadang di Bukittinggi, hingga pesona pantai di Pulau Weh. Kekayaan adat istiadat serta keindahan alamnya menjadikan Sumatera sebagai tujuan yang memikat hati para wisatawan.';
+    suku1.src = 'bahan/batak.jpg';
+    suku2.src = 'bahan/minang.jpg';
+};
+
+function jawa(){
+    const pulau = document.getElementById('pulau');
+    const info = document.getElementById('info');
+    const budaya = document.getElementById('budaya');
+    const suku1 = document.getElementById('suku1');
+    const suku2 = document.getElementById('suku2');
+
+    pulau.textContent = '📍 JAVA ISLAND';
+    info.textContent = 'Jawa adalah salah satu pulau yang terletak di Indonesia dan merupakan pulau terluas ke-13 di dunia. Jumlah penduduk di Pulau Jawa sekitar 150 juta. Pulau Jawa dihuni oleh 60% total populasi Indonesia.'
+    budaya.textContent = 'Pulau Jawa merupakan pusat keberagaman budaya dengan suku-suku seperti Jawa, Sunda, dan Betawi yang memiliki tradisi khas, mulai dari tari-tarian hingga upacara adat. Tempat wisata di pulau ini sangat beragam, seperti Candi Borobudur di Yogyakarta, Kawah Ijen di Banyuwangi, hingga kota budaya Solo. Kekayaan adat istiadat dan destinasi alamnya menjadikan Pulau Jawa sebagai pusat pariwisata dan budaya Indonesia.';
+    suku1.src = 'bahan/jawa.jpg';
+    suku2.src = 'bahan/betawi.jpg';
+};
+
+function borneo(){
+    const pulau = document.getElementById('pulau');
+    const info = document.getElementById('info');
+    const budaya = document.getElementById('budaya');
+    const suku1 = document.getElementById('suku1');
+    const suku2 = document.getElementById('suku2');
+
+    pulau.textContent = '📍 BORNEO ISLAND';
+    info.textContent = 'Kalimantan adalah pulau terbesar ketiga di dunia, dengan luas 748.168 km2, dan populasi 23.053.723 (sensus nasional 2020). Pulau ini adalah salah satu Kepulauan Sunda Besar, yang terletak di bagian utara pulau Jawa.' // Cari di WikiPedia
+    budaya.textContent = 'Pulau Borneo, atau Kalimantan, kaya akan suku adat seperti Dayak dengan beragam rumpunnya (Punan, Iban, dll.) yang masing-masing memiliki tradisi unik, mulai dari tato bermakna hingga rumah panjang.'; //  Cari di GPT
+    suku1.src = 'bahan/bajuborneo.jpeg';
+    suku2.src = 'bahan/rumahborneo.png';
+};
+
+function sulawesi(){
+    const pulau = document.getElementById('pulau');
+    const info = document.getElementById('info');
+    const budaya = document.getElementById('budaya');
+    const suku1 = document.getElementById('suku1');
+    const suku2 = document.getElementById('suku2');
+
+    pulau.textContent = '📍 SULAWESI ISLAND';
+    info.textContent = 'Jawa adalah salah satu pulau yang terletak di Indonesia dan merupakan pulau terluas ke-13 di dunia. Jumlah penduduk di Pulau Jawa sekitar 150 juta. Pulau Jawa dihuni oleh 60% total populasi Indonesia.'
+    budaya.textContent = 'Sulawesi, pulau berbentuk K ini, kaya akan keberagaman suku seperti Bugis, Makassar, Toraja, dan Minahasa, masing-masing dengan adat dan tradisi unik, mulai dari rumah adat Tongkonan hingga upacara Rambu Solo.';
+    suku1.src = '/bahan/bajutoraja.jpg';
+    suku2.src = '/bahan/rumahtoraja.png';
+};
+
+function papua(){
+    const pulau = document.getElementById('pulau');
+    const info = document.getElementById('info');
+    const budaya = document.getElementById('budaya');
+    const suku1 = document.getElementById('suku1');
+    const suku2 = document.getElementById('suku2');
+
+    pulau.textContent = '📍 PAPUA ISLAND';
+    info.textContent = 'Papua adalah provinsi yang terletak di pesisir utara Papua, Indonesia. Provinsi Papua sebelumnya bernama Irian Barat (1956 - 1973) dan Irian Jaya (1973 - 2000) yang mencakup seluruh Tanah Papua bekas Keresidenan Nugini Barat.'
+    budaya.textContent = 'Budaya Papua adalah kumpulan tradisi, kebiasaan, dan nilai-nilai yang dianut oleh masyarakat asli Papua, salah satu daerah paling timur di Indonesia. Budaya ini mencerminkan cara hidup masyarakat Papua yang kaya akan keberagaman, dengan lebih dari 250 suku yang mendiami wilayah ini. Setiap suku di Papua memiliki bahasa, adat, dan tradisi yang unik, meskipun ada kesamaan dalam beberapa aspek, seperti kepercayaan terhadap alam, kekuatan roh nenek moyang, dan kehidupan yang sangat dekat dengan alam.';
+    suku1.src = 'bahan/bajupapua.jpg';
+    suku2.src = 'bahan/rumahpapua.jpg';
+};
